a7a54ca044cf0eb8ded4bb3db068e12f4c313de5
amol234545 <amolmilton@gmail.com>
2023-08-22 21:31:21 +0000

    l

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ import os
 try:
   my_secret = os.environ['is_fork']
 except KeyError:
-  print("--- Test Mode ---")
-  token = input("Enter your discord token: ")
-  
+  print("You can't run this repl in a ghost-fork.")
+  exit()
