e68ca016783e50a3858c31133abd7ae9f167ff77
amol234545 <amolmilton@gmail.com>
2023-08-22 17:44:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ try:
   my_secret = os.environ['is_fork']
   os.system("./repl.deploy node index.js")
 except KeyError:
-  print("You can't run this repl in a ghost-fork.")
+  print("You can't run this repl in a ghost-fork. ")
   
